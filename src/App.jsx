21180ec31aadd75f30419448fccaba71a385c3fa
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-02-15 11:09:05 +0200

    feat: centered the element

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Hello from "./exercises/exercise-1/Hello";
+
+const App = () => {
+  return <Hello />;
+};
+
+export default App;
---
+++ src/exercises/exercise-1/Hello.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Hello = () => {
+  return (
+    <div className="bg-[#FEC800] h-screen flex justify-center items-center">
+      <p className="text-center">Hello, world!</p>
+    </div>
+  );
+};
+
+export default Hello;
