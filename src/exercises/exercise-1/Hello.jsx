7204ca0002e197d62ab15b7e44f0c5ad850fa2a4
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-02-15 12:32:43 +0200

    fix: deployemnt version

--- src/exercises/exercise-1/Hello.jsx
+++ src/exercises/exercise-1/Hello.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Hello = () => {
   return (
     <div className="bg-[#FEC800] h-screen flex justify-center items-center">
-      <p className="text-center">Hello, world!</p>
+      <p className="text-center text-base">Hello, world!</p>
     </div>
   );
 };
